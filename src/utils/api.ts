9c65dc02b302482ecbdf0e74faca63b74acb4f52
AAQILSHAIKH <shaikhaaqeel97@gmail.com>
2026-08-13 20:02:57 +0530

    updated build and url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import type { AxiosResponse, AxiosError } from "axios";
 
-const baseUrl: string = "http://localhost:8000";
-// const baseUrl: string = "https://www.indocscare.com:8000/";
+// const baseUrl: string = "http://localhost:8000";
+const baseUrl: string = "https://api.nabameditour.com";
 
 const imageBaseUrl: string = "http://localhost:8000";
 // const imageBaseUrl: string = "https://www.indocscare.com:8000";
